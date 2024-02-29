36ec20775dd7e154f9690ebeaea6f73dc19f9708
Arthur Torres <arthurtorres75@gmail.com>
2024-02-29 18:38:40 -0400

    control flow component @input new features

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
   templateUrl: './title.component.html',
-  styleUrl: './title.component.css'
 })
 export class TitleComponent {
-
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow!: boolean;
 }
